1a486e26e554f536b8426b9f3b60789fa5a043f2
Sascha Wessel <sawe@users.sf.net>
2007-05-04 18:45:44 +0000

    Use imlib's image cache

--- src/widget_image.c
+++ src/widget_image.c
@@ -36,30 +36,40 @@
 typedef struct {
 	xsg_angle_t *angle;
 	char *filename;
-	Imlib_Image image;
-	bool scale;
 } image_t;
 
 /******************************************************************************/
 
 static void render_image(xsg_widget_t *widget, Imlib_Image buffer, int up_x, int up_y) {
 	image_t *image;
+	Imlib_Image img;
 
 	xsg_debug("render_image");
 
 	image = (image_t *) widget->data;
 
+	img = xsg_imlib_load_image(image->filename);
+
+	if (unlikely(img == NULL)) {
+		xsg_warning("Cannot load image \"%s\"", image->filename);
+		return;
+	}
+
 	imlib_context_set_image(buffer);
 
-	if (image->angle)
-		imlib_blend_image_onto_image_at_angle(image->image, 1, 0, 0,
+	if ((image->angle == NULL) || (image->angle->angle == 0.0)) {
+		imlib_blend_image_onto_image(img, 1, 0, 0,
+				widget->width, widget->height, widget->xoffset - up_x,
+				widget->yoffset - up_y, widget->width, widget->height);
+	} else {
+		imlib_blend_image_onto_image_at_angle(img, 1, 0, 0,
 				image->angle->width, image->angle->height,
 				image->angle->xoffset - up_x, image->angle->yoffset - up_y,
 				image->angle->angle_x, image->angle->angle_y);
-	else
-		imlib_blend_image_onto_image(image->image, 1, 0, 0,
-				widget->width, widget->height, widget->xoffset - up_x,
-				widget->yoffset - up_y, widget->width, widget->height);
+	}
+
+	imlib_context_set_image(img);
+	imlib_free_image();
 }
 
 static void update_image(xsg_widget_t *widget, uint32_t var_id) {
@@ -81,8 +91,8 @@ void xsg_widget_image_parse() {
 	widget->update = 0;
 	widget->xoffset = xsg_conf_read_int();
 	widget->yoffset = xsg_conf_read_int();
-	widget->width = 0;
-	widget->height = 0;
+	widget->width = xsg_conf_read_uint();
+	widget->height = xsg_conf_read_uint();
 	widget->render_func = render_image;
 	widget->update_func = update_image;
 	widget->scroll_func = scroll_image;
@@ -90,51 +100,17 @@ void xsg_widget_image_parse() {
 
 	image->angle = NULL;
 	image->filename = xsg_conf_read_string();
-	image->image = NULL;
-	image->scale = FALSE;
-
-	image->image = xsg_imlib_load_image(image->filename);
-	if (unlikely(image->image == NULL))
-		xsg_error("Cannot load image \"%s\"", image->filename);
-
-	imlib_context_set_image(image->image);
-
-	widget->width = imlib_image_get_width();
-	widget->height = imlib_image_get_height();
 
 	while (!xsg_conf_find_newline()) {
 		if (xsg_conf_find_command("Angle")) {
 			angle = xsg_conf_read_double();
-		} else if (xsg_conf_find_command("Scale")) {
-			image->scale = TRUE;
-			widget->width = xsg_conf_read_uint();
-			widget->height = xsg_conf_read_uint();
 		} else {
-			xsg_conf_error("Angle or Scale");
+			xsg_conf_error("Angle");
 		}
 	}
 
-	if (image->scale) {
-		image->image = imlib_create_cropped_scaled_image(0, 0, imlib_image_get_width(),
-				imlib_image_get_height(), widget->width, widget->height);
-		imlib_free_image();
-	}
-
-	if (angle != 0.0) {
-		image->angle = xsg_angle_parse(angle, widget->xoffset, widget->yoffset,
-					&widget->width, &widget->height);
-
-		if (image->angle->angle == 90.0) {
-			imlib_image_orientate(1);
-			image->angle = NULL;
-		} else if (image->angle->angle == 180.0) {
-			imlib_image_orientate(2);
-			image->angle = NULL;
-		} else if (image->angle->angle == 270.0) {
-			imlib_image_orientate(3);
-			image->angle = NULL;
-		}
-	}
+	if (angle != 0.0)
+		image->angle = xsg_angle_parse(angle, widget->xoffset, widget->yoffset, &widget->width, &widget->height);
 
 	xsg_widgets_add(widget);
 }
