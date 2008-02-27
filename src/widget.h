6254b59c7251d30bb285cf8cfcde976d8b1061af
Sascha Wessel <sawe@users.sf.net>
2008-02-27 14:53:34 +0000

    Copyright

--- src/widget.h
+++ src/widget.h
@@ -1,7 +1,7 @@
 /* widget.h
  *
  * This file is part of xsysguard <http://xsysguard.sf.net>
- * Copyright (C) 2005 Sascha Wessel <sawe@users.sf.net>
+ * Copyright (C) 2005-2008 Sascha Wessel <sawe@users.sf.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
