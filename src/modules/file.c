010e9d8b535c179c3f2ab460cfadb6ddeb6056bb
Sascha Wessel <sawe@users.sf.net>
2008-03-14 17:50:20 +0000

    Added file tests

--- src/modules/file.c
+++ src/modules/file.c
@@ -27,15 +27,23 @@
 
 /******************************************************************************/
 
-typedef struct _file_t {
+typedef struct _file_buffer_t {
 	char *filename;
 	uint64_t update;
 	xsg_buffer_t *buffer;
-} file_t;
+} file_buffer_t;
+
+typedef struct _file_test_t {
+	char *filename;
+	uint64_t update;
+	xsg_file_test_t test;
+	double value;
+} file_test_t;
 
 /******************************************************************************/
 
-static xsg_list_t *file_list = NULL;
+static xsg_list_t *file_buffer_list = NULL;
+static xsg_list_t *file_test_list = NULL;
 
 /******************************************************************************/
 
@@ -43,9 +51,9 @@ static xsg_buffer_t *
 find_file_buffer(const char *filename, uint64_t update)
 {
 	xsg_list_t *l;
-	file_t *f;
+	file_buffer_t *f;
 
-	for (l = file_list; l; l = l->next) {
+	for (l = file_buffer_list; l; l = l->next) {
 		f = l->data;
 
 		if (!strcmp(f->filename, filename) && f->update == update) {
@@ -53,34 +61,66 @@ find_file_buffer(const char *filename, uint64_t update)
 		}
 	}
 
-	f = xsg_new(file_t, 1);
+	f = xsg_new(file_buffer_t, 1);
 
 	f->filename = xsg_strdup(filename);
 	f->update = update;
 	f->buffer = xsg_buffer_new();
 
-	file_list = xsg_list_append(file_list, (void *) f);
+	file_buffer_list = xsg_list_append(file_buffer_list, (void *) f);
 
 	return f->buffer;
+}
+
+static void *
+add_file_test(const char *filename, uint64_t update, xsg_file_test_t test)
+{
+	xsg_list_t *l;
+	file_test_t *f;
+
+	for (l = file_test_list; l; l = l->next) {
+		f = l->data;
+
+		if (!strcmp(f->filename, filename) && f->test == test) {
+			if ((update % f->update) == 0) {
+				return (void *) f;
+			}
+			if ((f->update % update) == 0) {
+				f->update = update;
+				return (void *) f;
+			}
+		}
+	}
+
+	f = xsg_new(file_test_t, 1);
+
+	f->filename = xsg_strdup(filename);
+	f->update = update;
+	f->test = test;
+	f->value = DNAN;
+
+	file_test_list = xsg_list_append(file_test_list, (void *) f);
+
+	return (void *) f;
 }
 
 /******************************************************************************/
 
 static void
-update_file(file_t *f)
+update_file_buffer(file_buffer_t *f)
 {
 	int fd;
 	int e;
 #if 0
 	if (!xsg_file_test(f->filename, XSG_FILE_TEST_IS_REGULAR)) {
-		xsg_warning("Not a regular file: %s", f->filename);
+		xsg_message("Not a regular file: %s", f->filename);
 		return;
 	}
 #endif
 	fd = open(f->filename, O_RDONLY | O_NONBLOCK);
 
 	if (fd == -1) {
-		xsg_warning("cannot open file %s: %s", f->filename,
+		xsg_message("cannot open file %s: %s", f->filename,
 				strerror(errno));
 		return;
 	}
@@ -106,7 +146,7 @@ update_file(file_t *f)
 		}
 
 		if (n == -1) {
-			xsg_warning("cannot read from file %s: %s", f->filename,
+			xsg_message("cannot read from file %s: %s", f->filename,
 					strerror(errno));
 			break;
 		}
@@ -122,17 +162,40 @@ update_file(file_t *f)
 }
 
 static void
+update_file_test(file_test_t *f)
+{
+	f->value = xsg_file_test(f->filename, f->test) ? 1.0 : 0.0;
+}
+
+static void
 update_files(uint64_t update)
 {
 	xsg_list_t *l;
 
-	for (l = file_list; l; l = l->next) {
-		file_t *f = l->data;
+	for (l = file_buffer_list; l; l = l->next) {
+		file_buffer_t *f = l->data;
 
 		if (update % f->update == 0) {
-			update_file(f);
+			update_file_buffer(f);
 		}
 	}
+
+	for (l = file_test_list; l; l = l->next) {
+		file_test_t *f = l->data;
+
+		if (update % f->update == 0) {
+			update_file_test(f);
+		}
+	}
+}
+
+/******************************************************************************/
+
+static double
+get_file_test(void *arg)
+{
+	file_test_t *f = (file_test_t *) arg;
+	return f->value;
 }
 
 /******************************************************************************/
@@ -147,15 +210,32 @@ parse_file(
 )
 {
 	char *filename;
-	xsg_buffer_t *buffer;
 
 	filename = xsg_conf_read_string();
 
-	buffer = find_file_buffer(filename, update);
+	if (xsg_conf_find_command("exists")) {
+		*arg = add_file_test(filename, update, XSG_FILE_TEST_EXISTS);
+		*num = get_file_test;
+	} else if (xsg_conf_find_command("is_regular")) {
+		*arg = add_file_test(filename, update, XSG_FILE_TEST_IS_REGULAR);
+		*num = get_file_test;
+	} else if (xsg_conf_find_command("is_symlink")) {
+		*arg = add_file_test(filename, update, XSG_FILE_TEST_IS_SYMLINK);
+		*num = get_file_test;
+	} else if (xsg_conf_find_command("is_dir")) {
+		*arg = add_file_test(filename, update, XSG_FILE_TEST_IS_DIR);
+		*num = get_file_test;
+	} else if (xsg_conf_find_command("is_executable")) {
+		*arg = add_file_test(filename, update, XSG_FILE_TEST_IS_EXECUTABLE);
+		*num = get_file_test;
+	} else {
+		xsg_buffer_t *buffer;
+
+		buffer = find_file_buffer(filename, update);
+		xsg_buffer_parse(buffer, NULL, num, str, arg);
+	}
 
 	xsg_free(filename);
-
-	xsg_buffer_parse(buffer, NULL, num, str, arg);
 
 	xsg_main_add_update_func(update_files);
 }
@@ -170,6 +250,17 @@ help_file(void)
 	} else {
 		xsg_string_truncate(string, 0);
 	}
+
+	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
+			"<filename>", "exists");
+	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
+			"<filename>", "is_regular");
+	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
+			"<filename>", "is_symlink");
+	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
+			"<filename>", "is_dir");
+	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
+			"<filename>", "is_executable");
 
 	xsg_buffer_help(string, XSG_MODULE_NAME, "<filename>");
 
