f734cf4933a41ad35ea59308cb13aa3dfa096188
Sascha Wessel <sawe@users.sf.net>
2008-04-03 15:59:47 +0000

    Removed file test stuff

--- src/modules/file.c
+++ src/modules/file.c
@@ -19,8 +19,6 @@
  */
 
 #include <xsysguard.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
@@ -33,17 +31,9 @@ typedef struct _file_buffer_t {
 	xsg_buffer_t *buffer;
 } file_buffer_t;
 
-typedef struct _file_test_t {
-	char *filename;
-	uint64_t update;
-	xsg_file_test_t test;
-	double value;
-} file_test_t;
-
 /******************************************************************************/
 
 static xsg_list_t *file_buffer_list = NULL;
-static xsg_list_t *file_test_list = NULL;
 
 /******************************************************************************/
 
@@ -72,38 +62,6 @@ find_file_buffer(const char *filename, uint64_t update)
 	return f->buffer;
 }
 
-static void *
-add_file_test(const char *filename, uint64_t update, xsg_file_test_t test)
-{
-	xsg_list_t *l;
-	file_test_t *f;
-
-	for (l = file_test_list; l; l = l->next) {
-		f = l->data;
-
-		if (!strcmp(f->filename, filename) && f->test == test) {
-			if ((update % f->update) == 0) {
-				return (void *) f;
-			}
-			if ((f->update % update) == 0) {
-				f->update = update;
-				return (void *) f;
-			}
-		}
-	}
-
-	f = xsg_new(file_test_t, 1);
-
-	f->filename = xsg_strdup(filename);
-	f->update = update;
-	f->test = test;
-	f->value = DNAN;
-
-	file_test_list = xsg_list_append(file_test_list, (void *) f);
-
-	return (void *) f;
-}
-
 /******************************************************************************/
 
 static void
@@ -111,12 +69,12 @@ update_file_buffer(file_buffer_t *f)
 {
 	int fd;
 	int e;
-#if 0
+
 	if (!xsg_file_test(f->filename, XSG_FILE_TEST_IS_REGULAR)) {
 		xsg_message("Not a regular file: %s", f->filename);
 		return;
 	}
-#endif
+
 	fd = open(f->filename, O_RDONLY | O_NONBLOCK);
 
 	if (fd == -1) {
@@ -162,12 +120,6 @@ update_file_buffer(file_buffer_t *f)
 }
 
 static void
-update_file_test(file_test_t *f)
-{
-	f->value = xsg_file_test(f->filename, f->test) ? 1.0 : 0.0;
-}
-
-static void
 update_files(uint64_t tick)
 {
 	xsg_list_t *l;
@@ -179,23 +131,6 @@ update_files(uint64_t tick)
 			update_file_buffer(f);
 		}
 	}
-
-	for (l = file_test_list; l; l = l->next) {
-		file_test_t *f = l->data;
-
-		if (tick % f->update == 0) {
-			update_file_test(f);
-		}
-	}
-}
-
-/******************************************************************************/
-
-static double
-get_file_test(void *arg)
-{
-	file_test_t *f = (file_test_t *) arg;
-	return f->value;
 }
 
 /******************************************************************************/
@@ -210,30 +145,13 @@ parse_file(
 )
 {
 	char *filename;
+	xsg_buffer_t *buffer;
 
 	filename = xsg_conf_read_string();
 
-	if (xsg_conf_find_command("exists")) {
-		*arg = add_file_test(filename, update, XSG_FILE_TEST_EXISTS);
-		*num = get_file_test;
-	} else if (xsg_conf_find_command("is_regular")) {
-		*arg = add_file_test(filename, update, XSG_FILE_TEST_IS_REGULAR);
-		*num = get_file_test;
-	} else if (xsg_conf_find_command("is_symlink")) {
-		*arg = add_file_test(filename, update, XSG_FILE_TEST_IS_SYMLINK);
-		*num = get_file_test;
-	} else if (xsg_conf_find_command("is_dir")) {
-		*arg = add_file_test(filename, update, XSG_FILE_TEST_IS_DIR);
-		*num = get_file_test;
-	} else if (xsg_conf_find_command("is_executable")) {
-		*arg = add_file_test(filename, update, XSG_FILE_TEST_IS_EXECUTABLE);
-		*num = get_file_test;
-	} else {
-		xsg_buffer_t *buffer;
+	buffer = find_file_buffer(filename, update);
 
-		buffer = find_file_buffer(filename, update);
-		xsg_buffer_parse(buffer, NULL, num, str, arg);
-	}
+	xsg_buffer_parse(buffer, NULL, num, str, arg);
 
 	xsg_free(filename);
 
@@ -250,17 +168,6 @@ help_file(void)
 	}
 
 	string = xsg_string_new(NULL);
-
-	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
-			"<filename>", "exists");
-	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
-			"<filename>", "is_regular");
-	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
-			"<filename>", "is_symlink");
-	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
-			"<filename>", "is_dir");
-	xsg_string_append_printf(string, "N %s:%s:%s\n", XSG_MODULE_NAME,
-			"<filename>", "is_executable");
 
 	xsg_buffer_help(string, XSG_MODULE_NAME, "<filename>");
 
